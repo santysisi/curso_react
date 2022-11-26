8bd98fdf6a9bb3b08b6a9b2abf7d6cd779843333
Santiago San Martin <santysm00@gmai.com>
2022-11-25 21:09:11 -0300

    se sube la tarea 1 y 2 del modulo 3

--- ejercicio1/src/index.js
+++
@@ -1,9 +0,0 @@
-const backgrounds = ["#FF0000" , "#DAA520" , "#008000"];
-const body = document.querySelector("body");
-const cuadros = document.querySelectorAll(".flex-child");
-
-cuadros.forEach((cuadro , index) => {
-    cuadro.addEventListener("click" , () =>{
-        body.style.backgroundColor = backgrounds[index];
-    });
-})
